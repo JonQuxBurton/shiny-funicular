a7d97a36881e32c6411f90f1631e0d488f6ce24f
Jon Burton <jonburto@gmail.com>
2022-08-13 22:44:59 +0100

    Init

---
+++ JonFooWebApplication/JonFooWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JonFooWebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
